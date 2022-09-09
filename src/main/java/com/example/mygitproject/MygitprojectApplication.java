acb4bb3812ff11e0592c9d600e2f65db69942314
sindin <sina.dayani@gmail.com>
2022-09-10 02:35:01 +0430

    this is first commit

---
+++ src/main/java/com/example/mygitproject/MygitprojectApplication.java
@@ -0,0 +1,15 @@
+package com.example.mygitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygitprojectApplication.class, args);
+		System.out.println("hi sina");
+	}
+
+
+}
---
+++ src/test/java/com/example/mygitproject/MygitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
